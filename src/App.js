323eb1c5848c21c8853e8c8858383dff301c8e75
Lukasz Nogaj <lukanogaj@hotmail.co.uk  git config --global user.name lukanogaj>
2024-06-08 15:39:10 +0100

    media-custom-hook

--- src/App.js
+++ src/App.js
@@ -9,8 +9,10 @@ import Feedback from "./components/Feedback";
 import AboutMe from "./components/AboutMe";
 import Frame from "./components/Frame";
 import Footer from "./components/Footer";
+import { useRef } from "react";
 
 function App() {
+	// const windowSize = useRef([window.innerWidth, window.innerHeight]);
 	return (
 		<Layout>
 			<Navbar />
--- src/components/PortfolioText/index.jsx
+++ src/components/PortfolioText/index.jsx
@@ -1,16 +1,39 @@
 import styles from "./index.module.scss";
 import PortfolioLinks from "../PortfolioLinks";
 import dataForPortfolio from "../DataForWorkPortfolio";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useRef } from "react";
 const PortfolioText = () => {
+	//Function
+	// // Custom Hook for slicing the array
+	// const useDecreimentArray = () => {
+	// 	return dataForPortfolio.slice(4);
+	// };
+	// useDecreimentArray();
+	// const windowSize = useRef([window.innerWidth, window.innerHeight]);
+	const useSize = () => {
+		const [windowSize, setWindowSize] = useState([
+			window.innerHeight,
+			window.innerWidth,
+		]);
 
-  // Custom Hook for slicing the array
-	const useDecrementArray = () => {
-		return dataForPortfolio.slice(4);
+		useEffect(() => {
+			const windowSizeHandler = () => {
+				setWindowSize([window.innerWidth, window.innerHeight]);
+			};
+			window.addEventListener("resize", windowSizeHandler);
+			return () => {
+				window.removeEventListener("resize", windowSizeHandler);
+			};
+		}, []);
+		return windowSize;
 	};
+	const windowsize = useSize();
+
+	console.log(windowsize);
+
 
   
-	console.log(useDecrementArray());
 	return (
 		<div className={styles.portfolioText}>
 			{dataForPortfolio.map((card) => (
