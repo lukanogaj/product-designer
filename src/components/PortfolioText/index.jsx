9caa1204e6813c202d67019a66d7f2c5fc2d7d7a
Lukasz Nogaj <lukanogaj@hotmail.co.uk  git config --global user.name lukanogaj>
2024-06-19 12:39:08 +0100

    portfolio-respoinsive

--- src/components/PortfolioText/index.jsx
+++ src/components/PortfolioText/index.jsx
@@ -37,22 +37,4 @@ const PortfolioText = () => {
 
 export default PortfolioText;
 
-// import { useState, useEffect } from "react";
 
-// export const useSize = () => {
-// 	const [windowSize, setWindowSize] = useState([
-// 		window.innerHeight,
-// 		window.innerWidth,
-// 	]);
-
-// 	useEffect(() => {
-// 		const windowSizeHandler = () => {
-// 			setWindowSize([window.innerWidth, window.innerHeight]);
-// 		};
-// 		window.addEventListener("resize", windowSizeHandler);
-// 		return () => {
-// 			window.removeEventListener("resize", windowSizeHandler);
-// 		};
-// 	}, []);
-// 	return windowSize;
-// };
