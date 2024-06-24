f7f62db5c12b516907c67b590802dc3771112382
Lukasz Nogaj <lukanogaj@hotmail.co.uk  git config --global user.name lukanogaj>
2024-06-24 08:55:47 +0100

    work-efficiency-responsiive

--- src/components/WorkEfficiency/index.jsx
+++ src/components/WorkEfficiency/index.jsx
@@ -4,49 +4,51 @@ import classNames from "classnames";
 
 const WorkEfficiency = () => {
 	return (
-		<div className={styles.workEfficiency}>
-			<div className={styles.workStats_first}>
-				<div className={styles.headStats}>
-					<h1 className={styles.headStat}>90%</h1>
+		<div className={styles.container}>
+			<div className={styles.workEfficiency}>
+				<div className={styles.workStats_first}>
+					<div className={styles.headStats}>
+						<h1 className={styles.headStat}>90%</h1>
+					</div>
+					<div className={styles.statsParagraph}>
+						<p>
+							Job Success <br /> Score on Upwork{" "}
+						</p>
+					</div>
 				</div>
-				<div className={styles.statsParagraph}>
-					<p>
-						Job Success <br /> Score on Upwork{" "}
-					</p>
+				<div className={styles.statsIcon}>
+					<img
+						src={dawnIcon}
+						width="80px"
+						height="80px"
+						alt=""
+					/>
 				</div>
-			</div>
-			<div className={styles.statsIcon}>
-				<img
-					src={dawnIcon}
-					width="80px"
-					height="80px"
-					alt=""
-				/>
-			</div>
-			<div className={styles.workStats_second}>
-				<div className={styles.headStats}>
-					<h1 className={styles.headStat}>{">"}25.000</h1>
+				<div className={styles.workStats_second}>
+					<div className={styles.headStats}>
+						<h1 className={styles.headStat}>{">"}25.000</h1>
+					</div>
+					<div className={styles.statsParagraph}>
+						<p>Duplicates on Figma Commnunity</p>
+					</div>
 				</div>
-				<div className={styles.statsParagraph}>
-					<p>Duplicates on Figma Commnunity</p>
+				<div className={styles.statsIcon}>
+					<img
+						src={dawnIcon}
+						width="80px"
+						height="80px"
+						alt=""
+					/>
 				</div>
-			</div>
-			<div className={styles.statsIcon}>
-				<img
-					src={dawnIcon}
-					width="80px"
-					height="80px"
-					alt=""
-				/>
-			</div>
-			<div className={styles.workStats_third}>
-				<div className={styles.headStats}>
-					<h1 className={styles.headStat}>{">2K"}</h1>
-				</div>
-				<div className={styles.statsParagraph}>
-					<p>
-						In Finished <br /> Works
-					</p>
+				<div className={styles.workStats_third}>
+					<div className={styles.headStats}>
+						<h1 className={styles.headStat}>{">2K"}</h1>
+					</div>
+					<div className={styles.statsParagraph}>
+						<p>
+							In Finished <br /> Works
+						</p>
+					</div>
 				</div>
 			</div>
 		</div>
