6ce38b4cfb0b3aaefe10b4c9d6c2b63f5edcbb8e
Lukasz Nogaj <lukanogaj@hotmail.co.uk  git config --global user.name lukanogaj>
2024-06-25 15:15:03 +0100

    portfolio-final

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ function App() {
 	return (
 		<Layout>
 			<Hero />
-			{/* <WorkEfficiency /> */}
-			{/* <Portfolio /> */}
+			<WorkEfficiency />
+			<Portfolio />
 			{/* <Skills /> */}
 			{/* <Feedback /> */}
 			{/* <AboutMe /> */}
